2055ac7507c28dba6f7d205783e6d02c577edd80
2024125075 <lms9109lyb@gmail.com>
2026-04-20 21:57:48 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -11,16 +11,17 @@ int main(void){
         count++; // 그리고 입력할 때 1씩 카운트한다.
             
         if(answer > guess){ //만약 입력한 ansewer이 입력한 guess값보다 크다면
-          printf("%d<?",guess); // guess값으로 입력한 정수에 <?를 붙여 출력한다.
+          printf("%d<?\n",guess); // guess값으로 입력한 정수에 <?를 붙여 출력한다.
         }
         else if(answer < guess){ //if 조건문이 false고 만약 guess값이 answer값보다 클 때는
-            printf("%d>?", guess); // guess 값을 불러와 >?와 함께 출력한다.
+            printf("%d>?\n", guess); // guess 값을 불러와 >?와 함께 출력한다.
         }
         else { // 위 두개의 조건식이 모두 아닐 때는
-            printf("%d==?",guess); //guess값을 정수로 ==? 와 함께 출력한다.
+            printf("%d==?\n",guess); //guess값을 정수로 ==? 와 함께 출력한다.
         } // do 블록은 여기서 종료된다.
     } while(guess != answer); //do를 다 실행하고 난 뒤에는 while을 실행하는데 내가 맞추려고 입력한 값이 정답이 아니면 do로 다시 돌아가 반복한다.
     
-    printf("%d->", count);  // 여태까지 입력했던 수를 센 count를 불러와 출력을 한다.
+    printf("%d", count);  // 여태까지 입력했던 수를 센 count를 불러와 출력을 한다.
+    
     return 0;
 }
